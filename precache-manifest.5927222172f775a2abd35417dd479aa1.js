c81848dd28c6d08db8859a9c4ca86cb1fef99a09
Tim Mullender <t.j.mullender@gmail.com>
2020-10-12 21:54:45 +0100

    Updates

---
+++ precache-manifest.5927222172f775a2abd35417dd479aa1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b42342fa53e2417416380ba5e044e3f9",
+    "url": "/cv/index.html"
+  },
+  {
+    "revision": "eebc4104a055f54e417c",
+    "url": "/cv/static/css/main.a610ea47.chunk.css"
+  },
+  {
+    "revision": "a98fe87306bd4ae5daf5",
+    "url": "/cv/static/js/2.57b7407b.chunk.js"
+  },
+  {
+    "revision": "89b0379e7bcda1a468d8b0343aeb4e53",
+    "url": "/cv/static/js/2.57b7407b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eebc4104a055f54e417c",
+    "url": "/cv/static/js/main.0de0ea59.chunk.js"
+  },
+  {
+    "revision": "9e0e921495960402e4a4",
+    "url": "/cv/static/js/runtime-main.c17789b0.js"
+  }
+]);
